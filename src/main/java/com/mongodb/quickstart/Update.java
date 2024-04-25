4bd10d11112ed5230db39cfebb397a052505e41f
ct_code <77695884+c-tapp@users.noreply.github.com>
2024-04-25 00:45:15 -0400

    Update Update.java

--- src/main/java/com/mongodb/quickstart/Update.java
+++ src/main/java/com/mongodb/quickstart/Update.java
@@ -32,6 +32,41 @@ public class Update {
             System.out.println("=> Updating the doc with {\"student_id\":10003}. Adding comment.");
             System.out.println(gradesCollection.find(filter).first().toJson(prettyPrint));
             System.out.println(updateResult);
+
+            // upsert
+            filter = and(eq("student_id", 10004d), eq("class_id", 10d));
+            updateOperation = push("comments", "Upserting = Update + inserting.");
+            UpdateOptions options = new UpdateOptions().upsert(true);
+            updateResult = gradesCollection.updateOne(filter, updateOperation, options);
+            System.out.println("\n=> Upsert document with {\"student_id\":10004.0, \"class_id\": 10.0} because it doesn't exist yet.");
+            System.out.println(updateResult);
+            System.out.println(gradesCollection.find(filter).first().toJson(prettyPrint));
+
+            // update many documents
+            filter = eq("student_id", 10003);
+            updateResult = gradesCollection.updateMany(filter, updateOperation);
+            System.out.println("\n=> Updating all the documents with {\"student_id\":10003}.");
+            System.out.println(updateResult);
+
+            // findOneAndUpdate
+            filter = eq("student_id", 10000);
+            Bson update1 = inc("x", 10); // increment x by 10. As x doesn't exist yet, x=10.
+            Bson update2 = rename("class_id", "new_class_id"); // rename variable "class_id" in "new_class_id".
+            Bson update3 = mul("scores.0.score", 2); // multiply the first score in the array by 2.
+            Bson update4 = addToSet("comments", "This comment is uniq"); // creating an array with a comment.
+            Bson update5 = addToSet("comments", "This comment is uniq"); // using addToSet so no effect.
+            Bson updates = combine(update1, update2, update3, update4, update5);
+            // returns the old version of the document before the update.
+            Document oldVersion = gradesCollection.findOneAndUpdate(filter, updates);
+            System.out.println("\n=> FindOneAndUpdate operation. Printing the old version by default:");
+            System.out.println(oldVersion.toJson(prettyPrint));
+
+            // but I can also request the new version
+            filter = eq("student_id", 10001);
+            FindOneAndUpdateOptions optionAfter = new FindOneAndUpdateOptions().returnDocument(ReturnDocument.AFTER);
+            Document newVersion = gradesCollection.findOneAndUpdate(filter, updates, optionAfter);
+            System.out.println("\n=> FindOneAndUpdate operation. But we can also ask for the new version of the doc:");
+            System.out.println(newVersion.toJson(prettyPrint));
         }
     }
 }
